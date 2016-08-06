8d702ae7ffb1a018c3858c9ddd6bc03ceccdcfac
kaleb <zarlo5899@gmail.com>
2016-08-06 18:10:02 +1000

    login Work

--- Chat/Chat.Win/Login.cs
+++ Chat/Chat.Win/Login.cs
@@ -27,10 +27,11 @@ namespace Chat.Win
         {
             //ChatForm CF = new ChatForm();
             //CF.Show();
-            WebRequest request = WebRequest.Create("http://punksky.xyz/games/user/login.json");
+
+            WebRequest request = WebRequest.Create("http://punksky.xyz/games/user/token.json");
 
             request.Method = "POST";
-            byte[] byteArray = Encoding.UTF8.GetBytes(@"{""username"":""" + textBox_UserName.Text + @""",""password"":'" + textBox_Password.Text + @"""");
+            byte[] byteArray = Encoding.UTF8.GetBytes("");
             request.ContentType = "application/json";
             request.ContentLength = byteArray.Length;
             Stream dataStream = request.GetRequestStream();
@@ -40,6 +41,31 @@ namespace Chat.Win
             dataStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(dataStream);
             string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+
+            responseFromServer = responseFromServer.Remove(0, 10);
+            responseFromServer = responseFromServer.Remove(responseFromServer.Length - 2,2);
+            MessageBox.Show(responseFromServer);
+            request = WebRequest.Create("http://punksky.xyz/games/user/login.json");
+
+            string Data = "{\"username\":\"" + textBox_UserName.Text + "\",\"password\":\"" + textBox_Password.Text + "\"}";
+            MessageBox.Show(Data);
+            request.Method = "POST";
+             byteArray = Encoding.UTF8.GetBytes(Data);
+            request.Headers.Add("X-CSRF-Token", responseFromServer);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+             dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+             response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+             reader = new StreamReader(dataStream);
+             responseFromServer = reader.ReadToEnd();
             MessageBox.Show(responseFromServer);
             reader.Close();
             dataStream.Close();
